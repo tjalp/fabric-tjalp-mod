602d11f3250a84dc566635a964e159d4697dbc55
Butterness <tiesalkemade@gmail.com>
2021-05-09 13:15:33 +0200

    Initial commit

---
+++ src/main/java/net/tjalp/mod/tjalp/Tjalp.java
@@ -0,0 +1,11 @@
+package net.tjalp.mod.tjalp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tjalp implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
