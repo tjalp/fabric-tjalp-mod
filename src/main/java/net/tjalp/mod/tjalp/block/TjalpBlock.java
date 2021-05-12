5b4caf6047c24111d6334cbc78dcb89275427d68
Butterness <tiesalkemade@gmail.com>
2021-05-12 14:28:39 +0200

    Remove ruby items

--- src/main/java/net/tjalp/mod/tjalp/block/TjalpBlock.java
+++ src/main/java/net/tjalp/mod/tjalp/block/TjalpBlock.java
@@ -1,16 +1,13 @@
 package net.tjalp.mod.tjalp.block;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.tjalp.mod.tjalp.Tjalp;
 
 public class TjalpBlock {
 
-    public static Block RUBY_BLOCK;
+
 
     private static Block register(String id, Block block) {
         return register(new Identifier(Tjalp.MOD_ID, id), block);
@@ -21,6 +18,6 @@ public class TjalpBlock {
     }
 
     public static void registerBlocks() {
-        RUBY_BLOCK = register("ruby_block", new Block(AbstractBlock.Settings.of(Material.METAL).requiresTool().strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL)));
+
     }
 }
